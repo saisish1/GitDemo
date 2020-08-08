fb1227264f180c3a6c5cbfb78cfca83c21cdc04d
saisish <saisish324@gmail.com>
2020-08-08 15:24:17 +0530

    first commit

---
+++ src/test/java/Project1/AppiumTest.java
@@ -0,0 +1,16 @@
+package Project1;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void AndroidApps()
+	{
+		System.out.println("AndroidApps");
+	}
+	@Test
+	public void IOsApps()
+	{
+		System.out.println("IOsApps");
+	}
+}
---
+++ src/test/java/Project1/RestAPITest.java
@@ -0,0 +1,17 @@
+package Project1;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void PostJira()
+	{
+		System.out.println("PostJira");	
+	}
+	@Test
+	public void DeleteTwiter()
+	{
+		System.out.println("DeleteTwiter");
+	}
+
+}
